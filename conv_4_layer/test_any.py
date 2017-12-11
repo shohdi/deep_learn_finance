381ca91fa317efa5ae90e4596235805bdc29f9b7
shohdi elsheemy <shohdi@gmail.com>
2017-12-11 07:38:36 +0300

    last today before work

--- conv_4_layer/test_any.py
+++ conv_4_layer/test_any.py
@@ -7,7 +7,7 @@ from train.forex_fix import ForexFix
 def main(_):
     test = ForexFix(60,15)
     print("history : " , test.history , " future : " , test.future )
-
+    test.getInputOutput()
 
 
 main('')
--- conv_4_layer/train/forex_fix.py
+++ conv_4_layer/train/forex_fix.py
@@ -6,6 +6,15 @@ class ForexFix:
     def __init__(self,history,future):
         self.history = history
         self.future = future
+        self.readFile = ReadFile("/home/shohdi/projects/learning-tensorflow/projects/deep_learn_finance/conv_4_layer/input/myOldData.csv")
+    
+
+
+    def getInputOutput(self):
+        mainArr = self.readFile.readMultiFiles()
+        print(mainArr)
+
+        
 
 
 
