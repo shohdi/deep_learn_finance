0a0fe0b399b12c7a1a881193aa16c76bd0b4feb9
shohdi elsheemy <shohdi@gmail.com>
2017-12-16 14:17:34 +0300

    still working in arranging input and result

--- conv_4_layer/test_any.py
+++ conv_4_layer/test_any.py
@@ -1,18 +1,19 @@
 from train.forex_divide_input_output import ForexDivideInputOutput
 from train.input_average import InputAverage
 
-
+#1/1 - 1/50 - 1/100 - 1/500 - 1/1000
 
 
 def main(_):
     INPUT_SIZE = 60;
     OUTPUT_SIZE = 15;
-    test = ForexDivideInputOutput(INPUT_SIZE,OUTPUT_SIZE,"/home/shohdi/projects/learning-tensorflow/projects/deep_learn_finance/conv_4_layer/input/myOldData.csv")
+    FILE_NAMES = "/home/shohdi/projects/learning-tensorflow/projects/deep_learn_finance/conv_4_layer/input/myOldData.csv";
+    test = ForexDivideInputOutput(INPUT_SIZE,OUTPUT_SIZE,FILE_NAMES);
     #print("history : " , test.history , " future : " , test.future )
-    inputTuble,outputTuble,mainArr = test.getInputOutput()
+    inputTuble,outputTuble,mainArr = test.getInputOutput();
     iAvg = InputAverage();
     average = iAvg.getInputAverage(OUTPUT_SIZE,inputTuble,mainArr);
-
+    print('average ' , average);
     #print("input ",inputTuble," output ",outputTuble)
 
 main('')
--- conv_4_layer/train/input_average.py
+++ conv_4_layer/train/input_average.py
@@ -13,6 +13,10 @@ class InputAverage:
             inputArr = arr[inputTuble[i][0]:inputTuble[i][1]];
             inputArr = inputArr[:,2:];
             inputAverage = 0.0;
+            valueToWin = 0.0;
+            valueToTrade = 0.0;
+            multiplyValue = 0.0;
+
             count = 0.0;
             for index in range(len(inputArr)):
                 if((index+outputSize) <= len(inputArr)):
@@ -25,6 +29,21 @@ class InputAverage:
         
             inputAverage = inputAverage/count;
             inputAverage = inputAverage * 0.7;
-            ret.append(inputAverage);
+            valueToWin,valueToTrade,multiplyValue = self.calculateValueToTrade(inputAverage);
+            ret.append((inputAverage,multiplyValue,valueToTrade,valueToWin));
         return np.array(ret);
+
+    def calculateValueToTrade(self,inputAverage):
+        #1/1 - 1/50 - 1/100 - 1/500 - 1/1000
+        xArr = [1/1000.0,1/500.0,1/100.0,1/50.0,1/1.0];
+        for i in range(len(xArr)):
+            tradeX = xArr[i];
+            if(inputAverage <= tradeX):
+                valueToTrade = tradeX/inputAverage;
+                valueToWin = 1/valueToTrade;
+                valueToTrade = round(valueToTrade,2);
+                valueToWin = round(valueToWin,2);
+                return valueToWin,valueToTrade,tradeX;
+
+        return 0.0,0.0,0.0;
 
