5e07d143164db809d19bf66379cc7abfdc2df0cc
shohdi elsheemy <shohdi@gmail.com>
2017-12-25 15:02:18 +0300

    return missing layer , problem was that gpu not detected

--- conv_4_layer/train/keras_helper.py
+++ conv_4_layer/train/keras_helper.py
@@ -37,12 +37,12 @@ class KerasHelper:
 
         model.add(Dropout(0.2));
 
-        #model.add(Conv2D(128,kernel_size=5,padding="same"));
-        #model.add(Activation('relu'));
+        model.add(Conv2D(128,kernel_size=5,padding="same"));
+        model.add(Activation('relu'));
 
-        #model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)));
+        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)));
 
-        #model.add(Dropout(0.2));
+        model.add(Dropout(0.2));
 
         model.add(Flatten());
         model.add(Dense(1024));
