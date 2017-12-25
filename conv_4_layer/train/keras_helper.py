3cc9760c159efd1167aa9c5b7edbd677eb9e1057
shohdi elsheemy <shohdi@gmail.com>
2017-12-25 23:33:52 +0300

    save statistics instead of showing it

--- conv_4_layer/train/keras_helper.py
+++ conv_4_layer/train/keras_helper.py
@@ -92,14 +92,16 @@ class KerasHelper:
         plt.ylabel('accuracy');
         plt.xlabel('epoch');
         plt.legend(['train','test'],loc='upper left');
-        plt.show();
+        #plt.show();
+        plt.savefig(os.path.join(outputDir,'acc.png'));
         plt.plot(history.history['loss']);
         plt.plot(history.history['val_loss']);
         plt.title('model loss');
         plt.ylabel('loss');
         plt.xlabel('epoch');
         plt.legend(['train','test'],loc='upper left');
-        plt.show();
+        #plt.show();
+        plt.savefig(os.path.join(outputDir,'loss.png'));
 
 
         
