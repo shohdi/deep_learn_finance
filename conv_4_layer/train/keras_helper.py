a29605bcfe4c83a7de6121f003772ee514d0458b
shohdi elsheemy <shohdi@gmail.com>
2017-12-26 22:10:42 +0300

    add more layers

--- conv_4_layer/train/keras_helper.py
+++ conv_4_layer/train/keras_helper.py
@@ -22,11 +22,17 @@ class KerasHelper:
         model.add(Conv2D(  32,kernel_size=5,padding="same",input_shape=input_shape));
         model.add(Activation('relu'));
 
+        model.add(Conv2D(  32,kernel_size=5,padding="same",input_shape=input_shape));
+        model.add(Activation('relu'));
+
         
 
         model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)));
 
         model.add(Dropout(0.2));
+
+        model.add(Conv2D(64,kernel_size=5,padding="same"));
+        model.add(Activation('relu'));
 
         model.add(Conv2D(64,kernel_size=5,padding="same"));
         model.add(Activation('relu'));
@@ -40,11 +46,17 @@ class KerasHelper:
         model.add(Conv2D(128,kernel_size=5,padding="same"));
         model.add(Activation('relu'));
 
+        model.add(Conv2D(128,kernel_size=5,padding="same"));
+        model.add(Activation('relu'));
+
         model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)));
 
         model.add(Dropout(0.2));
 
         model.add(Flatten());
+        model.add(Dense(1024));
+        model.add(Activation('relu'));
+
         model.add(Dense(1024));
         model.add(Activation('relu'));
 
