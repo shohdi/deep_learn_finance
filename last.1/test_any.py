34712bc2eaab8c3f77cb050ae7c6300926605450
shohdi elsheemy <shohdi@gmail.com>
2018-01-18 07:34:31 +0300

    add dropout to convolution net

--- last.1/test_any.py
+++ last.1/test_any.py
@@ -6,20 +6,20 @@ import os as os;
 
 from train.deep_input_ret import DeepInputRet
 
-
+from train.keras_helper import KerasHelper
 
 from train.join_input import JoinInput
 
 flags = tf.app.flags;
 FLAGS = flags.FLAGS;
-flags.DEFINE_string('shohdi_debug','True','shohdi_debug');
+flags.DEFINE_string('shohdi_debug','False','shohdi_debug');
 flags.DEFINE_integer('INPUT_SIZE',60,'INPUT_SIZE');
 flags.DEFINE_integer('OUTPUT_SIZE',15,'OUTPUT_SIZE');
 flags.DEFINE_integer('HOW_MANY_MINUTES',1,'HOW_MANY_MINUTES');
 flags.DEFINE_string('INPUT_FOLDER','input','INPUT_FOLDER');
 
 
-flags.DEFINE_integer('npEpoch',20,'npEpoch');
+flags.DEFINE_integer('npEpoch',200,'npEpoch');
 
 flags.DEFINE_integer('batchSize',128,'batchSize');
 
@@ -30,6 +30,8 @@ flags.DEFINE_string('outputDir','output','outputDir');
 flags.DEFINE_string('inputTrainData','','inputTrainData');
 flags.DEFINE_string('trainFiles','myOldData.csv','trainFiles');
 flags.DEFINE_string('testFiles','myOldData.csv','testFiles');
+
+flags.DEFINE_bool('isOperation',True,'isOperation');
 
 #1/1 - 1/50 - 1/100 - 1/500 - 1/1000
 
@@ -45,10 +47,19 @@ def main(_):
 
     
     
+        
+
     inputClass = DeepInputRet(FLAGS.INPUT_SIZE,FLAGS.OUTPUT_SIZE,trainFileNames,FLAGS.HOW_MANY_MINUTES);
+    
     xTrain,yTrain,xTest,yTest = inputClass.getAllResultsEqual(False,FLAGS.valSplit);
     
 
+    
+
+
+    helper = KerasHelper();
+
+    helper.convNetTrain(xTrain,yTrain,xTest,yTest,FLAGS.npEpoch,FLAGS.batchSize,FLAGS.valSplit,FLAGS.outputDir,FLAGS.inputTrainData);
     
    
     
--- last.1/train/keras_helper.py
+++ last.1/train/keras_helper.py
@@ -41,21 +41,29 @@ class KerasHelper:
         '''
         model.add(Conv2D(  32,kernel_size=4,padding="same",input_shape=input_shape));
         model.add(Activation("relu"));
+        
         model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)));
+
 
         model.add(Conv2D(  64,kernel_size=4,padding="same",input_shape=input_shape));
         model.add(Activation("relu"));
+       
         model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)));
+
+        model.add(Dropout(0.2));
 
         model.add(Conv2D(  128,kernel_size=4,padding="same",input_shape=input_shape));
         model.add(Activation("relu"));
+        
         model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)));
 
-        
+        model.add(Dropout(0.2));
 
         model.add(Flatten());
         model.add(Dense(1024));
+        
         model.add(Activation('relu'));
+        model.add(Dropout(0.2));
         model.add(Dense(3));
         
         model.add(Activation('softmax'));
