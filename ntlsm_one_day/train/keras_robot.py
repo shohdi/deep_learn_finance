2647357c35a8db762160d84496d291fade4393b2
shohdi elsheemy <shohdi@gmail.com>
2018-05-04 01:41:53 +0300

    add file to seperate flags

--- ntlsm_one_day/train/keras_robot.py
+++ ntlsm_one_day/train/keras_robot.py
@@ -10,40 +10,18 @@ import os as os;
 
 from train.join_input import JoinInput
 from train.read_file import ReadFile
-
-flags = tf.app.flags;
-FLAGS = flags.FLAGS;
-flags.DEFINE_string('shohdi_debug','False','shohdi_debug');
-flags.DEFINE_integer('INPUT_SIZE',2880,'INPUT_SIZE');
-flags.DEFINE_integer('OUTPUT_SIZE',96,'OUTPUT_SIZE');
-flags.DEFINE_integer('HOW_MANY_MINUTES',1,'HOW_MANY_MINUTES');
-flags.DEFINE_string('INPUT_FOLDER','input','INPUT_FOLDER');
+from train.my_flags import MyFlags
 
 
-flags.DEFINE_integer('npEpoch',5,'npEpoch');
-
-flags.DEFINE_integer('batchSize',50,'batchSize');
-
-flags.DEFINE_float('valSplit',0.05,'valSplit');
-
-flags.DEFINE_string('outputDir','output','outputDir');
-
-flags.DEFINE_string('inputTrainData','','inputTrainData');
-#flags.DEFINE_string('trainFiles','15_year.csv','trainFiles');
-flags.DEFINE_string('trainFiles','last_year.csv','trainFiles');
-
-flags.DEFINE_string('testFiles','last_year.csv','testFiles');
-
-flags.DEFINE_bool('isOperation',True,'isOperation');
 
 #1/1 - 1/50 - 1/100 - 1/500 - 1/1000
 
 
 def main(_):
-    
+    myFlags = MyFlags();
     joinInput = JoinInput();
-    trainFileNames = joinInput.joinInput(FLAGS.INPUT_FOLDER,FLAGS.trainFiles);
-    testFileNames = joinInput.joinInput(FLAGS.INPUT_FOLDER,FLAGS.testFiles);
+    trainFileNames = joinInput.joinInput(myFlags.INPUT_FOLDER,myFlags.trainFiles);
+    testFileNames = joinInput.joinInput(myFlags.INPUT_FOLDER,myFlags.testFiles);
 
 
     readFile = ReadFile();
@@ -52,14 +30,14 @@ def main(_):
 
     
     #loop on train
-    end = FLAGS.INPUT_SIZE + FLAGS.OUTPUT_SIZE + 0 ;
+    end = myFlags.INPUT_SIZE + myFlags.OUTPUT_SIZE + 0 ;
     arr=np.zeros((end,));
     for i in range(len(trainArr)- end) : 
         start = i;
-        end = FLAGS.INPUT_SIZE + FLAGS.OUTPUT_SIZE + i ;
+        end = myFlags.INPUT_SIZE + myFlags.OUTPUT_SIZE + i ;
         oneInputOutput = trainArr[start:end];
         oneInputOutput = oneInputOutput.copy();
-        oneInput = oneInputOutput[0: FLAGS.INPUT_SIZE];
+        oneInput = oneInputOutput[0: myFlags.INPUT_SIZE];
         npArr = np.array(oneInputOutput);
         arr = npArr;
     
---
+++ ntlsm_one_day/train/my_flags.py
@@ -0,0 +1,54 @@
+import numpy as np
+import tensorflow as tf
+
+import os as os;
+
+
+
+
+
+from train.join_input import JoinInput
+from train.read_file import ReadFile
+
+flags = tf.app.flags;
+FLAGS = flags.FLAGS;
+flags.DEFINE_string('shohdi_debug','False','shohdi_debug');
+flags.DEFINE_integer('INPUT_SIZE',2880,'INPUT_SIZE');
+flags.DEFINE_integer('OUTPUT_SIZE',96,'OUTPUT_SIZE');
+flags.DEFINE_integer('HOW_MANY_MINUTES',1,'HOW_MANY_MINUTES');
+flags.DEFINE_string('INPUT_FOLDER','input','INPUT_FOLDER');
+
+
+flags.DEFINE_integer('npEpoch',5,'npEpoch');
+
+flags.DEFINE_integer('batchSize',50,'batchSize');
+
+flags.DEFINE_float('valSplit',0.05,'valSplit');
+
+flags.DEFINE_string('outputDir','output','outputDir');
+
+flags.DEFINE_string('inputTrainData','','inputTrainData');
+#flags.DEFINE_string('trainFiles','15_year.csv','trainFiles');
+flags.DEFINE_string('trainFiles','last_year.csv','trainFiles');
+
+flags.DEFINE_string('testFiles','last_year.csv','testFiles');
+
+flags.DEFINE_bool('isOperation',True,'isOperation');
+
+
+
+class MyFlags:
+    def __init__(self):
+        self.shohdi_debug = FLAGS.shohdi_debug;
+        self.INPUT_SIZE = FLAGS.INPUT_SIZE;
+        self.OUTPUT_SIZE = FLAGS.OUTPUT_SIZE;
+        self.HOW_MANY_MINUTES = FLAGS.HOW_MANY_MINUTES;
+        self.INPUT_FOLDER = FLAGS.INPUT_FOLDER;
+        self.npEpoch = FLAGS.npEpoch;
+        self.batchSize = FLAGS.batchSize;
+        self.valSplit = FLAGS.valSplit;
+        self.outputDir = FLAGS.outputDir;
+        self.inputTrainData = FLAGS.inputTrainData;
+        self.trainFiles = FLAGS.trainFiles;
+        self.testFiles = FLAGS.testFiles;
+        self.isOperation = FLAGS.isOperation;
