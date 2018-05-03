ed9c6e9a5abafd0cbea9e1119dc63eb3e0bffddc
shohdi elsheemy <shohdi@gmail.com>
2018-05-04 01:30:00 +0300

    remove not needed files

--- ntlsm_one_day/train/keras_robot.py
+++ ntlsm_one_day/train/keras_robot.py
@@ -4,9 +4,9 @@ import tensorflow as tf
 
 import os as os;
 
-from train.deep_input_ret import DeepInputRet
 
-from train.keras_helper import KerasHelper
+
+
 
 from train.join_input import JoinInput
 from train.read_file import ReadFile
@@ -14,13 +14,13 @@ from train.read_file import ReadFile
 flags = tf.app.flags;
 FLAGS = flags.FLAGS;
 flags.DEFINE_string('shohdi_debug','False','shohdi_debug');
-flags.DEFINE_integer('INPUT_SIZE',2400,'INPUT_SIZE');
-flags.DEFINE_integer('OUTPUT_SIZE',80,'OUTPUT_SIZE');
-flags.DEFINE_integer('HOW_MANY_MINUTES',80,'HOW_MANY_MINUTES');
+flags.DEFINE_integer('INPUT_SIZE',2880,'INPUT_SIZE');
+flags.DEFINE_integer('OUTPUT_SIZE',96,'OUTPUT_SIZE');
+flags.DEFINE_integer('HOW_MANY_MINUTES',1,'HOW_MANY_MINUTES');
 flags.DEFINE_string('INPUT_FOLDER','input','INPUT_FOLDER');
 
 
-flags.DEFINE_integer('npEpoch',20,'npEpoch');
+flags.DEFINE_integer('npEpoch',5,'npEpoch');
 
 flags.DEFINE_integer('batchSize',50,'batchSize');
 
@@ -29,7 +29,9 @@ flags.DEFINE_float('valSplit',0.05,'valSplit');
 flags.DEFINE_string('outputDir','output','outputDir');
 
 flags.DEFINE_string('inputTrainData','','inputTrainData');
-flags.DEFINE_string('trainFiles','15_year.csv','trainFiles');
+#flags.DEFINE_string('trainFiles','15_year.csv','trainFiles');
+flags.DEFINE_string('trainFiles','last_year.csv','trainFiles');
+
 flags.DEFINE_string('testFiles','last_year.csv','testFiles');
 
 flags.DEFINE_bool('isOperation',True,'isOperation');
@@ -48,8 +50,20 @@ def main(_):
     trainArr = readFile.readMultiFiles(trainFileNames);
     testArr = readFile.readMultiFiles(testFileNames);
 
-    print(trainArr[0:100]);
-    print(testArr[0:100]);
+    
+    #loop on train
+    end = FLAGS.INPUT_SIZE + FLAGS.OUTPUT_SIZE + 0 ;
+    arr=np.zeros((end,));
+    for i in range(len(trainArr)- end) : 
+        start = i;
+        end = FLAGS.INPUT_SIZE + FLAGS.OUTPUT_SIZE + i ;
+        oneInputOutput = trainArr[start:end];
+        oneInputOutput = oneInputOutput.copy();
+        oneInput = oneInputOutput[0: FLAGS.INPUT_SIZE];
+        npArr = np.array(oneInputOutput);
+        arr = npArr;
+    
+    print (arr.shape,arr);
 
 
 
@@ -57,20 +71,10 @@ def main(_):
     
         
 
-    inputClass = DeepInputRet(FLAGS.INPUT_SIZE,FLAGS.OUTPUT_SIZE,trainFileNames,FLAGS.HOW_MANY_MINUTES);
     
-    xTrain,yTrain,xTest,yTest = inputClass.getAllResultsEqual(False,FLAGS.valSplit);
-    
-
-    
-
-
-    helper = KerasHelper();
-
-    helper.convNetTrain(xTrain,yTrain,xTest,yTest,FLAGS.npEpoch,FLAGS.batchSize,FLAGS.valSplit,FLAGS.outputDir,FLAGS.inputTrainData);
     
    
     
 
 if __name__ == '__main__':
-    tf.app.run()
+    tf.app.run();
