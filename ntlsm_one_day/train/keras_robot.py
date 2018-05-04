f6fba9649d2641373cffe01591783bc5814952bc
shohdi elsheemy <shohdi@gmail.com>
2018-05-05 01:09:04 +0300

    still working on input

--- ntlsm_one_day/train/keras_robot.py
+++ ntlsm_one_day/train/keras_robot.py
@@ -8,10 +8,9 @@ import os as os;
 
 
 
-from train.join_input import JoinInput
-from train.read_file import ReadFile
+
 from train.my_flags import MyFlags
-from train.normalize_input import NormalizeInput;
+from train.main_input_loop import MainInputLoop;
 
 
 
@@ -20,37 +19,11 @@ from train.normalize_input import NormalizeInput;
 
 def main(_):
     myFlags = MyFlags();
-    joinInput = JoinInput();
-    trainFileNames = joinInput.joinInput(myFlags.INPUT_FOLDER,myFlags.trainFiles);
-    testFileNames = joinInput.joinInput(myFlags.INPUT_FOLDER,myFlags.testFiles);
-
-
-    readFile = ReadFile();
-    trainArr = np.array( readFile.readMultiFiles(trainFileNames));
-    testArr = np.array( readFile.readMultiFiles(testFileNames));
-    trainArr = trainArr.reshape(-1,6);
-    testArr = testArr.reshape(-1,6);
-    print(trainArr.shape);
-    print(testArr.shape);
-    normalizeInput = NormalizeInput();
-    #loop on train
-    end = (myFlags.INPUT_SIZE) + (myFlags.OUTPUT_SIZE) + 0 ;
-    arr=np.zeros((end,));
-    for i in range(len(trainArr)- end) : 
-        start = i;
-        end = myFlags.INPUT_SIZE + myFlags.OUTPUT_SIZE + i ;
-        oneInputOutput = trainArr[start:end];
-        oneInputOutput = oneInputOutput.copy();
-        oneInput = oneInputOutput[0: myFlags.INPUT_SIZE];
-        npArr = np.array(oneInputOutput);
-        
-        npArr = npArr[:,2:];
-        if(i%1000 == 0):
-            print(npArr);
-            print(normalizeInput.getHighLowClose(npArr))
-        arr = npArr;
     
-    print (arr.shape,arr);
+    mainInputLoop = MainInputLoop();
+
+    mainInputLoop.normalizeInput(myFlags.trainFiles);
+    
 
 
 
---
+++ ntlsm_one_day/train/main_input_loop.py
@@ -0,0 +1,39 @@
+import numpy as np
+import tensorflow as tf
+
+import os as os;
+
+
+from train.my_flags import MyFlags
+from train.normalize_input import NormalizeInput;
+
+from train.one_in_out_prep import OneInOutPrep;
+from train.join_input import JoinInput
+from train.read_file import ReadFile
+
+
+class MainInputLoop:
+    def __init__(self):
+        self.myFlags = MyFlags();
+        self.oneInOutPrep = OneInOutPrep();
+        self.joinInput = JoinInput();
+        self.readFile = ReadFile();
+
+    def normalizeInput(self,files):
+        
+        fileNames = self.joinInput.joinInput(self.myFlags.INPUT_FOLDER,files);
+               
+        arr = np.array( self.readFile.readMultiFiles(fileNames));
+        size = int( len(arr)/self.myFlags.candleSize);
+        for i in range(size):
+            index = i * self.myFlags.candleSize;
+
+            oneInOut = self.oneInOutPrep.fixOneInputOutput(arr,index);
+            if(i == (size-1)):
+                print(oneInOut);
+
+
+
+
+        
+        
