45a7ad2ad6de541b04b5c2fb5854e6acdc041ad0
Shohdy ElShemy <shohdi@gmail.com>
2018-05-25 02:03:44 +0300

    finish make it like cards one up and one down

--- ntlsm_one_day/train/main_input_loop.py
+++ ntlsm_one_day/train/main_input_loop.py
@@ -27,19 +27,40 @@ class MainInputLoop:
         size = int( len(arr)//self.myFlags.candleSize);
         samplesSize = size- (self.myFlags.INPUT_SIZE + self.myFlags.OUTPUT_SIZE);
         X = np.zeros((samplesSize,self.myFlags.noOfShots+1,self.myFlags.INPUT_SIZE - self.myFlags.noOfShots,3),dtype='float32');
+
+        xUp = np.zeros((samplesSize,self.myFlags.noOfShots+1,self.myFlags.INPUT_SIZE - self.myFlags.noOfShots,3),dtype='float32');
+        xDown = np.zeros((samplesSize,self.myFlags.noOfShots+1,self.myFlags.INPUT_SIZE - self.myFlags.noOfShots,3),dtype='float32');
+        upCount = 0;
+        downCount = 0;
         Y = np.zeros((samplesSize,1),dtype='float32');
         for i in range(samplesSize):
             index = i * self.myFlags.candleSize;
 
             oneX,oneY = self.oneInOutPrep.fixOneInputOutput(arr,index);
             oneX = np.array(oneX,dtype='float32');
-            X[i] = oneX;
-            oneY = np.array([oneY],dtype='float32');
-            Y[i] = oneY;
+            if(oneY > 0.5):
+                xUp[upCount] = oneX;
+                upCount = upCount + 1 ;
+            else :
+                xDown[downCount] = oneX;
+                downCount = downCount + 1;
+        myCount = downCount;
+        if(downCount > upCount):
+            myCount = upCount;
+        
+        for i in range(myCount):
+            index = i * 2;
+            X[index] = xUp[i];
+            Y[index] = np.array([1.0],dtype='float32');
+            X[index+1] = xDown[i];
+            Y[index+1] =  np.array([0.0],dtype='float32');
+            
             if(i % 1000 == 0):
-                print ('input number : %d , output %f' % (i,oneY));
+                print ('input number : %d , output %f' % (index,Y[index]));
             
 
+        X = X[:(myCount * 2)];
+        Y = Y[:(myCount * 2)];
         
 
         print ('x shape %s , y shape %s' % (X.shape,Y.shape));
