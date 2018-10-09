e64eea973ebeb7d17a9ffb00fc90822c2da3ef7f
Shohdy ElShemy <shohdi@gmail.com>
2018-10-09 22:26:19 +0300

    add some logic to random events to make some profit to learn on it

--- forex_reinforcement/forex_environment.py
+++ forex_reinforcement/forex_environment.py
@@ -14,10 +14,11 @@ class ForexEnvironment:
         self._last_state = None;
         self._last_reward = 0;
         self._last_game_over = False;
+        self._myRandomFrame = 0;
 
         
         
-       
+      
     
 
     def reset(self):
@@ -46,7 +47,7 @@ class ForexEnvironment:
 
         #print("step ended  ",self._step_started , self._step_ended);
         self._states_coll.append(self._last_state);
-        
+
         return self._last_state,self._last_reward,self._last_game_over,None;
     
 
@@ -54,9 +55,29 @@ class ForexEnvironment:
         ret  = int( math.floor( (np.random.random() * 4)));
         if(len(self._states_coll) > 0):
             if(self._last_state[0,5] > 0 or self._last_state[0,4] > 0):
+                self._myRandomFrame = self._myRandomFrame + 1;
                 ret = int( math.floor( (np.random.random() * 3000)));
+                '''
+                lastClose = self._last_state[0,3];
+                up = self._last_state[0,4];
+                down = self._last_state[0,5];
+
+                if(up > 0 and (lastClose - up) >= 0.0005):
+                    ret = 3;
+                elif(down > 0 and (down - lastClose) >= 0.0005):
+                    ret = 3;
+                '''
+                if(self._myRandomFrame > 2000):
+                    if(self._last_state[0,4] > 0 and self._last_state[0,3] > self._last_state[0,4]):
+                        ret = 3;
+                    elif(self._last_state[0,5] > 0 and self._last_state[0,3] < self._last_state[0,5]):
+                        ret = 3;
+                
+
+            else:
+                self._myRandomFrame = 0;
         
-        if(ret > 4):
+        if(ret > 3):
             ret = 0;
 
         return ret;
